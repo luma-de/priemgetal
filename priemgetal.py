4ae1cee5248a41f8716a4b0218e8dc9bb93d86c1
luma-de <luc.maussen@gmail.com>
2025-10-26 11:23:02 +0100

    first commit

---
+++ priemgetal.py
@@ -0,0 +1,12 @@
+num = int(input("Voer een getal in: "))
+
+if num > 1:
+    for i in range(2, num):
+        if (num % i) == 0:
+            print(num, "is geen priemgetal")
+            print(i, "keer", num//i, "is", num)
+            break
+    else:
+        print(num, "is een priemgetal")
+else:
+    print(num, "is geen priemgetal")
