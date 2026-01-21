e1c42d5232b3b57f014735d2828906a248c804e5
luma-de <luc.maussen@gmail.com>
2026-01-21 12:44:19 +0100

    Update priemgetal.py

--- priemgetal.py
+++ priemgetal.py
@@ -4,7 +4,6 @@ if num > 1:
     for i in range(2, num):
         if (num % i) == 0:
             print(num, "is geen priemgetal")
-            print(i, "keer", num//i, "is", num)
             break
     else:
         print(num, "is een priemgetal")
